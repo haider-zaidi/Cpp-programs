188ea81345d83af175e312cff73804ef669f16f3
Haider Zaidi <115361024+haider-zaidi@users.noreply.github.com>
2023-10-08 20:18:51 +0530

    Lecture4-CipherSchools

---
+++ C++ Code/Lecture4-CipherSchools.cpp
@@ -0,0 +1,19 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+
+    int a=10;
+    int b=20;
+    int c;
+
+    c=a+b;
+    cout<<c<<endl<<"a+b";
+
+    cout<<a+b;
+    cout<<endl;
+    cout<<"a+b";
+
+    return 0;
+
+}
