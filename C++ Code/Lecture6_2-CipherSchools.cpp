47fae3d1ccffaeefeff5f83acaf98d7cfff54e98
Haider Zaidi <115361024+haider-zaidi@users.noreply.github.com>
2023-10-08 23:07:09 +0530

    Lecture6_2-CipherSchools

---
+++ C++ Code/Lecture6_2-CipherSchools.cpp
@@ -0,0 +1,24 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+
+    // int i=0;
+    // int j=0;
+    // int k=0;
+
+    // cout<<i<<" "<<j<<" "<<k;
+    // j=i++;
+    // cout<<i<<" "<<j<<" "<<k;
+    // k=++i;
+    // cout<<i<<" "<<j<<" "<<k;
+
+    int i=0;
+    for(i=1;i<101;++i){
+        cout<<i<<" ";
+    }
+
+
+    return 0;
+
+}
